18f8fb86aa2b6d8c266de5a06e40b0ee1deea762
Noelia Donato <noeliadonato@gmail.com>
2023-07-28 21:39:52 +0200

    refactor: update url onrender

--- src/service/planet.ts
+++ src/service/planet.ts
@@ -1,20 +1,20 @@
-import type { Planet } from "../types/planet";
+import type { Planet } from '../types/planet'
 
-const URL = "https://api-planets.onrender.com/api/planet";
+const URL = 'https://planetas-api.onrender.com/api/planet'
 
 function getErrorMessage(error: unknown) {
-  if (error instanceof Error) return error.message;
-  return String(error);
+  if (error instanceof Error) return error.message
+  return String(error)
 }
 
 export const ServicePlanets = {
   get: async function (name: string): Promise<Planet> {
     try {
-      const response = await fetch(`${URL}/${name}`);
-      const data = await response.json();
-      return data as Planet;
+      const response = await fetch(`${URL}/${name}`)
+      const data = await response.json()
+      return data as Planet
     } catch (e) {
-      throw new Error(getErrorMessage(e));
+      throw new Error(getErrorMessage(e))
     }
   }
-};
+}
